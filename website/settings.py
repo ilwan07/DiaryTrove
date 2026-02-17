ade9977822309648685e4cec2fba44117c8da06f
ilwan07 <miloudilwan@gmail.com>
2026-02-17 17:31:10 +0100

    changed smtp config

--- website/settings.py
+++ website/settings.py
@@ -154,14 +154,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = '[smtp host here]'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
-CONTACT_EMAIL = os.getenv('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = os.getenv('AGENT_EMAIL')
+CONTACT_EMAIL = os.getenv('CONTACT_EMAIL')
 
 # Some URLs
 
